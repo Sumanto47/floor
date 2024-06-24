e3f33c0662afddf4563812b7c499ce4904994588
Sumanto47 <sumantochatterjeenavi47@gmail.com>
2024-06-24 18:22:21 +0530

    Delete assets directory

--- assets/js/main/select.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var selects = document.querySelectorAll('.page_Select');
-    selects.forEach(function(select) {
-        select.addEventListener('change', function() {
-            var selectedValue = this.value;
-            window.location.href = selectedValue;
-        });
-    });
-});
