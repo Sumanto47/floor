b03c1afbc315dca61a460e3c929224fbaaacb39d
Sumanto47 <sumantochatterjeenavi47@gmail.com>
2024-06-24 20:02:40 +0530

    Update modal.js

--- assets/js/main/modal.js
+++ assets/js/main/modal.js
@@ -1,15 +1,45 @@
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// Get the buttons that open the modal
+var btns = document.getElementsByClassName("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks a button, open the modal
+for (var i = 0; i < btns.length; i++) {
+  btns[i].onclick = function() {
+    modal.style.display = "block";
+  }
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -23,3 +53,4 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
